60ab7d48d63aa175074a4b3e36ea806d0e618390
renarsuurpere <renar.suurpere@khk.ee>
2021-10-06 15:16:42 +0300

    As a user I can log in/out [Delivers #179816152]

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express()
-const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('docs/swagger.yaml')
-require("dotenv").config()
+
+require('dotenv').config()
 
 // Serve API documentation on /docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
@@ -13,8 +13,9 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // Parse request body as JSON into req.body
 app.use(express.json())
 
-// users endpoints
+// endpoints
 app.use('/users', require('./routes/users'))
+app.use('/sessions', require('./routes/sessions'))
 
 mongoose.connect(process.env.MONGO_URL, {}, function (){
     console.log('Connected to mongoDB')
---
+++ models/Session.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('Session', mongoose.Schema({
+        userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
+    }
+    ,
+    {
+        toJSON: {
+            transform: (docIn, docOut) => {
+                docOut.id = docOut._id
+                delete docOut.__v
+                delete docOut._id
+            }
+        }
+    }
+))
